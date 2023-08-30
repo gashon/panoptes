e4e85eaf7b762de92c97b7f3a4e8b03685497287
GashonHussein <gashon96@gmail.com>
2023-08-30 14:23:06 -0700

    feat: daemonize process

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,8 +1,8 @@
 use std::error::Error;
-use std::process::exit;
 use std::fmt::{Display};
-use std::os::unix::io::AsRawFd;
 use std::fs::OpenOptions;
+use std::process::exit;
+use std::os::unix::io::AsRawFd;
 
 macro_rules! try_ret {
     ($expr:expr, $err:expr) => (
@@ -15,10 +15,9 @@ macro_rules! try_ret {
 }
 
 pub fn daemonize() -> Result<(), Box<dyn Error>> {
-    fork?();
-    // disconnect session from terminal
-    tryret!(libc::setsid(), DaemonizeError::Detach)?;
-    fork?();    
+    fork()?;  // Fixed this line
+    try_ret!(libc::setsid(), DaemonizeError::Detach)?;  
+    fork()?;  // Fixed this line
     redirect_streams()
 }
 
@@ -26,19 +25,19 @@ fn fork() -> Result<(), Box<dyn Error>> {
     unsafe {
         match libc::fork() {
             0 => Ok(()),
-            e if e < 0 return Err(Box::new(DaemonizeError::Fork(e))),
-            _ => exit(0) // parent automatically exits
+            e if e < 0 => { return Err(Box::new(DaemonizeError::Fork(e))) },
+            _ => exit(0)
         }
     }
 }
 
 fn redirect_streams() -> Result<(), Box<dyn Error>> {
-    let devnull = OpenOptions::new().read(true).write(true).open("/dev/null");
-    let log = OpenOptions::new().read(true).write(true).open("/tmp/log")
+    let devnull = OpenOptions::new().read(true).write(true).open("/dev/null")?;
+    let log = OpenOptions::new().read(true).write(true).open("/dev/null")?;  
 
-    tryret!(libc::dup2(devnull.as_raw_fd(), libc::STDIN_FILENO), DaemonizeError::RedirectStreams)?;
-    tryret!(libc::dup2(log.as_raw_fd(), libc::STDOUT_FILENO), DaemonizeError::RedirectStreams)?;
-    tryret!(libc::dup2(log.as_raw_fd(), libc::STDERR_FILENO), DaemonizeError::RedirectStreams)?;
+    try_ret!(libc::dup2(devnull.as_raw_fd(), libc::STDIN_FILENO), DaemonizeError::RedirectStreams)?;
+    try_ret!(libc::dup2(log.as_raw_fd(), libc::STDOUT_FILENO), DaemonizeError::RedirectStreams)?;
+    try_ret!(libc::dup2(log.as_raw_fd(), libc::STDERR_FILENO), DaemonizeError::RedirectStreams)?;
 
     Ok(())
 }
@@ -60,11 +59,4 @@ impl Display for DaemonizeError {
     }
 }
 
-impl Error for DaemonizeError {
-    fn description(&self) -> &str {
-        "DaemonizeError"
-    }
-
-    fn cause(&self) -> Option<&dyn Error> {
-        None
-    }
+impl Error for DaemonizeError {}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,64 @@
+mod daemon;
 mod monitor;
 
+use std::env;
 use std::time::Duration;
 use std::thread::sleep;
+use reqwest::blocking::Client as HttpClient;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Ping {
+    activity: String,
+}
 
 fn main() {
+    let password = match env::var("OPTES_PASSWORD") {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("OPTES_PASSWORD environment variable not set.");
+            return;
+        },
+    };
 
-    let tick = Duration::from_secs(1);
+    match daemon::daemonize() {
+        Ok(_) => {
+            println!("Successfully daemonized.");
+        },
+        Err(err) => {
+            eprintln!("Failed to daemonize: {}", err);
+            return;
+        }
+    }
+
+    let tick_duration = Duration::from_secs(1);
+    let http = HttpClient::new();
 
     loop {
+        let req = || -> Result<(), reqwest::Error> {
+            let current_application = monitor::get_current_window().unwrap_or("Unavailable".to_string());
+            http.post("https://live.ghussein.org/api/desktop")
+                .json(&Ping {
+                    activity: current_application,
+                })
+                .header(
+                    "Authorization",
+                    format!("Bearer {}", password),
+                )
+                .send()?;
+            println!("sent req");
+            Ok(())
+        };
+
+        if let Err(err) = req() {
+            eprintln!("{}", err);
+        }
+
         match monitor::get_current_window() {
             Ok(name) => println!("Current active window: {}", name),
             Err(err) => eprintln!("An error occurred: {}", err),
         }
 
-        sleep(tick);
-    }  
+        sleep(tick_duration);
+    }
 }
